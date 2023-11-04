f1c41bf8ec0dd24d120edf8a66dbf01dca01a09d
Zachary <ztbrosius@gmail.com>
2023-11-04 18:53:54 -0400

    Set up week 10 notebook and created a Fruits array

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] Fruits = {"Apple", "Orange", "Grape", "Strawberry", "Kiwi"};
+
+        for(int fruitCounter = 0; fruitCounter < Fruits.length; ++fruitCounter)
+            System.out.println("Fruit Name: " + Fruits[fruitCounter]);
+//            Fruits[fruitCounter] = "Fruit #:" + fruitCounter;
+
+    } // ends main method
+} // ends Main class
